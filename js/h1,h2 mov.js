d5b81baef7b8df341b14d11f41bdcb8f6eead983
Denis Lopez Sacher <dlopezsacher@cifpfbmoll.eu>
2025-01-24 21:00:48 +0100

    Añadido Aparicion/Desaparicion Del H1,H2
    He agregado la posibilidad para que aparezca y desparezca los titulos para hacer uso de JavaScript ademas de algo mas de visualidad

---
+++ js/h1,h2 mov.js
@@ -0,0 +1,26 @@
+const animatedHeadings = document.querySelectorAll('.animated-heading');
+
+function handleScroll() {
+    animatedHeadings.forEach( h1 => {
+        const rect = h1.getBoundingClientRect();
+        const inVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
+
+        if (inVisible) {
+            h1.classList.add('visible');
+            h1.classList.remove('hidden-top','hidden-bottom');
+        } else {
+            if ( rect.top < 0 ) {
+                h1.classList.add('hidden-top');
+                h1.classList.remove('visible', 'hidden-bottom');
+
+            } else if (rect.bottom > window.innerHeight ) {
+                h1.classList.add('hidden-bottom');
+                h1.classList.remove('visible', 'hidden-top');
+            }
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScroll);
+
+handleScroll();
